360c87be8c1744e584ef4febc28c88adc3e628e6
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2023-08-12 20:27:34 +0530

    Particle Container

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite, Container, Point } from 'pixi.js'
+import { Application, Sprite, Container, ParticleContainer } from 'pixi.js'
 
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,23 +9,15 @@ const app = new Application({
     height: window.innerHeight
 });
 
+const particleConty: ParticleContainer = new ParticleContainer();
+// Pretty much everything that worked on a Container will work with a ParticleContainer.
+
 const conty: Container = new Container();
 conty.x = 0;
 conty.y = 0;
-app.stage.addChild(conty);
+app.stage.addChild(conty, particleConty);
 
-const clampy: Container = Sprite.from("clampy.png");
+const clampy: Sprite = Sprite.from("clampy.png");
 clampy.x = window.innerHeight / 2 - 50;
 clampy.y = window.innerWidth / 2 - 250;
-conty.addChild(clampy);
-
-const bigConty: Container = new Container();
-bigConty.scale.set(2); // You can use set and only one value to set x and y
-bigConty.position.x = 100;
-bigConty.y = 200; // this is a shortcut for .position.y and it also exists one for .position.x
-app.stage.addChild(bigConty);
-
-const littleConty: Container = new Container();
-// position has a copy setter. It won't use your reference but copy the values from it!
-littleConty.position = new Point(300,200);
-bigConty.addChild(littleConty);
+particleConty.addChild(clampy);
