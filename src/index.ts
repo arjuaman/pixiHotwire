f70a244069df9ef59e9c586177fa4bbaddd2ee09
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2023-08-12 17:00:09 +0530

    parent-child containers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite, Container } from 'pixi.js'
+import { Application, Sprite, Container, Point } from 'pixi.js'
 
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -18,3 +18,14 @@ const clampy: Container = Sprite.from("clampy.png");
 clampy.x = window.innerHeight / 2 - 50;
 clampy.y = window.innerWidth / 2 - 250;
 conty.addChild(clampy);
+
+const bigConty: Container = new Container();
+bigConty.scale.set(2); // You can use set and only one value to set x and y
+bigConty.position.x = 100;
+bigConty.y = 200; // this is a shortcut for .position.y and it also exists one for .position.x
+app.stage.addChild(bigConty);
+
+const littleConty: Container = new Container();
+// position has a copy setter. It won't use your reference but copy the values from it!
+littleConty.position = new Point(300,200);
+bigConty.addChild(littleConty);
