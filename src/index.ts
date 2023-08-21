35e50a76e16110e605ea0295823eba1388c88313
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2023-08-21 17:20:02 +0530

    Graphics

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite, Container, ParticleContainer } from 'pixi.js'
+import { Application, Container, ParticleContainer, Graphics } from 'pixi.js'
 
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -18,9 +18,16 @@ const particleConty: ParticleContainer = new ParticleContainer();
 
 app.stage.addChild(conty, particleConty);
 
-const clampy: Sprite = Sprite.from("clampy.png");
-clampy.x = window.innerHeight / 2;
-clampy.y = window.innerWidth / 2;
-clampy.anchor.set(0.5, 0.5);
+const graphy: Graphics = new Graphics();
 
-conty.addChild(clampy);
+// we give instructions in order. begin fill, line style, draw circle, end filling
+graphy.beginFill(0xFF00FF);
+graphy.lineStyle(10, 0x00FF00);
+graphy.drawCircle(0, 0, 25); // See how I set the drawing at 0,0? NOT AT 100, 100!
+graphy.endFill();
+
+app.stage.addChild(graphy); //I can add it before setting position, nothing bad will happen.
+
+// Here we set it at 100,100
+graphy.x = 100;
+graphy.y = 100;
