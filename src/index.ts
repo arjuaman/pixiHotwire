3ada2e0726505bf357641f6036b34357c3da709c
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2023-08-21 17:24:47 +0530

    Bitmap text and font

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Container, ParticleContainer, Graphics, TextStyle, Text } from 'pixi.js'
+import { Application, Container, ParticleContainer, Graphics, TextStyle, Text, BitmapFont, BitmapText } from 'pixi.js'
 
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -41,3 +41,25 @@ const texty: Text = new Text('私に気づいて先輩！', styly); // Text supp
 // texty.text = "This is expensive to change, please do not abuse";
 
 app.stage.addChild(texty);
+
+// If you need to know, this is the expensive part. This creates the font atlas
+BitmapFont.from("comic 32", {
+  fill: "#ffffff", // White, will be colored later
+  fontFamily: "Comic Sans MS",
+  fontSize: 32
+})
+
+// Remember, this font only has letters and numbers. No commas or any other symbol.
+const bitmapTexty: BitmapText = new BitmapText("I love baking, my family, and my friends",
+  {
+      fontName: "comic 32",
+      fontSize: 32, // Making it too big or too small will look bad
+      tint: 0xFF0000 // Here we make it red.
+  });
+
+bitmapTexty.text = "This is cheap";
+bitmapTexty.text = "Change it as much as you want";
+bitmapTexty.x = 300;
+bitmapTexty.y = 300;
+
+app.stage.addChild(bitmapTexty);
